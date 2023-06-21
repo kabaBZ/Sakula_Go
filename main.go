eeeffa84d442fe7f6ca7ec2d0f2cde99590832d6
guojiangbin <guojiangbin@nnuo.com>
2023-06-21 16:15:16 +0800

    import bug

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"github.com/kabaBZ/sakula_go/utils/get"
-	"github.com/kabaBZ/sakula_go/utils/post"
-	"github.com/kabaBZ/sakula_go/utils/struct"
-)
-
 func main() {
 	Struct()
 	Get()
